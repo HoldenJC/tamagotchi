d4745c164565d8577e801e83b6246a47e2779566
Holden Clark and Na Hyung Choi <holden+na@>
2019-07-01 11:00:06 -0700

    add tests and backend code for first 3 README.md specs

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,11 +1,54 @@
-// import { XXX } from './../src/backend-code.js';
-//
-// describe('XXX', function() {
-//
-//   it('should ...', function() {
-//     expect(triangle.side1).toEqual(3);
-//     expect(triangle.side2).toEqual(4);
-//     expect(triangle.side3).not.toEqual(6);
-//   });
-// 
-// })
+import { Pet } from './../src/backend-code.js';
+
+describe('Pet', function() {
+
+  let testPet;
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    testPet = new Pet();
+    testPet.reduceStats();
+  });
+
+  afterEach(function(){
+    jasmine.clock().uninstall();
+  });
+
+
+  it('should make a new Tamagotchi with max stats', function() {
+    expect(testPet.food).toEqual(30);
+    expect(testPet.play).toEqual(30);
+    expect(testPet.energy).toEqual(30);
+    expect(testPet.potty).toEqual(30);
+    expect(testPet.love).toEqual(30);
+    console.log(testPet);
+  });
+
+  it('should decrease all stat values by 1 after 10000 milliseconds', function(){
+
+    jasmine.clock().tick(10000);
+    expect(testPet.food).toEqual(29);
+    expect(testPet.play).toEqual(29);
+    expect(testPet.energy).toEqual(29);
+    expect(testPet.potty).toEqual(29);
+    expect(testPet.love).toEqual(29);
+    console.log(testPet);
+  });
+
+  it('should increase actioned stat by 5', function(){
+    jasmine.clock().tick(100000);
+    testPet.feed();
+    testPet.playWith();
+    testPet.rest();
+    testPet.bathroom();
+    testPet.lovePet();
+    expect(testPet.food).toEqual(25);
+    expect(testPet.play).toEqual(25);
+    expect(testPet.energy).toEqual(25);
+    expect(testPet.potty).toEqual(25);
+    expect(testPet.love).toEqual(25);
+    console.log(testPet);
+  });
+
+
+})
--- src/backend-code.js
+++ src/backend-code.js
@@ -1,1 +1,36 @@
+export class Pet {
+  constructor(){
+    this.food = 30;
+    this.play = 30;
+    this.energy = 30;
+    this.potty = 30;
+    this.love = 30;
+  }
 
+  reduceStats(){
+    setInterval(() => {
+      this.food--;
+      this.play--;
+      this.energy--;
+      this.potty--;
+      this.love--;
+    }, 10000);
+  }
+
+  feed(){
+    this.food += 5;
+  }
+  playWith(){
+    this.play += 5;
+  }
+  rest(){
+    this.energy += 5;
+  }
+  bathroom(){
+    this.potty += 5;
+  }
+  lovePet(){
+    this.love += 5;
+  }
+
+}
